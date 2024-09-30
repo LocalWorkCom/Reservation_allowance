bd26a8108bca0d460036499c3ce79673c80c77dc
unknown <eng.noda102gmail.com>
2024-09-30 13:39:58 +0300

    drop column from reservation_allowances

---
+++ database/migrations/2024_09_30_132729_add_sector_column_to_reservation_allowances.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::table('reservation_allowances', function (Blueprint $table) {
+            $table->unsignedBigInteger('sector_id')->nullable();
+            $table->foreign('sector_id')->references('id')->on('sectors')->onUpdate('cascade');
+
+            
+
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::table('reservation_allowances', function (Blueprint $table) {
+            //
+        });
+    }
+};
