1da91b01ac009d144626fc7667f37bd5e0ad536e
Rasha Shalan <rasha.shalan@brmaja.com>
2024-10-01 13:42:07 +0300

    fix header

--- resources/views/layout/header.blade.php
+++ resources/views/layout/header.blade.php
@@ -110,23 +110,6 @@
                         <ul>
                             <div class="row col-12">
                                 <div class="col-12">
-<<<<<<< HEAD
-                                        <li class="{{ request()->routeIs('reservation_allowances') ? 'active' : '' }}">
-                                            <img src="{{ asset('frontend/images/police.svg') }}" alt="logo"
-                                                style="margin-left: 7px;">
-                                            <a href="{{ route('reservation_allowances.index') }}">عرض بدل حجز</a>
-                                        </li>
-                                        <li class="{{ request()->routeIs('reservation_allowances.index') ? 'active' : '' }}">
-                                            <img src="{{ asset('frontend/images/police.svg') }}" alt="logo"
-                                                style="margin-left: 7px;">
-                                            <a href="{{ route('reservation_allowances.index') }}">رصيد بدل حجز</a>
-                                        </li>
-                                        <li class="{{ request()->routeIs('reservation_allowances.index') ? 'active' : '' }}">
-                                            <img src="{{ asset('frontend/images/police.svg') }}" alt="logo"
-                                                style="margin-left: 7px;">
-                                            <a href="{{ route('Reserv_statistic.index') }}">احصائيات بدل حجز</a>
-                                        </li>
-=======
                                     <li class="{{ request()->routeIs('reservation_allowances') ? 'active' : '' }}">
                                         <img src="{{ asset('frontend/images/police.svg') }}" alt="logo"
                                             style="margin-left: 7px;">
@@ -149,7 +132,6 @@
                                             style="margin-left: 7px;">
                                         <a href="{{ route('Reserv_statistic.index') }}">احصائيات بدل حجز</a>
                                     </li>
->>>>>>> f6ec169070bc4281611b534639341fa687aeb280
                                 </div>
                             </div>
                         </ul>
