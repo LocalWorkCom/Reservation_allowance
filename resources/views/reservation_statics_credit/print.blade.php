3aef90de24f78324d55f38cf48bfe49c1c11619f
Omnia <omnia_abdelkader@saeeh.com>
2024-10-01 14:55:31 +0300

    logo

--- resources/views/reservation_statics_credit/print.blade.php
+++ resources/views/reservation_statics_credit/print.blade.php
@@ -25,27 +25,71 @@
             margin: 0;
             font-size: 24px;
             color: #333;
+            flex: 1;
+            text-align: left; 
         }
         .header-box img {
-            width: 100px; /* Adjust image size */
+            width: 100px; 
             height: auto;
+            margin-right: 20px; 
+        }
+        table {
+            width: 80%; 
+            margin: 0 auto; 
+            border-collapse: collapse;
+            margin-top: 20px;
+        }
+        table, th, td {
+            border: 1px solid #ddd;
+            padding: 12px;
+            text-align: center;
+        }
+        th {
+            background-color: #f2f2f2;
+        }
+        td {
+            font-size: 18px;
         }
     </style>
 </head>
 <body>
 
     <div class="header-box">
+        <img src="{{ asset('img/logo.png') }}" alt="Logo"> 
         <h1>احصائيات بدل حجز يوم {{ $dayOfWeek }} من شهر {{ $currentMonth }}</h1>
-        <img src="{{ asset('img/logo.png') }}" alt="Logo">
     </div>
 
-    <p>التاريخ: {{ $date }}</p>
-    <p>حجز جزئي العدد: {{ $partial_reservation_count }}</p>
-    <p>حجز جزئي المبلغ: {{ $partial_reservation_amount }}</p>
-    <p>حجز كلي العدد: {{ $full_reservation_count }}</p>
-    <p>حجز كلي المبلغ: {{ $full_reservation_amount }}</p>
-    <p>اجمالي المبلغ: {{ $total_amount }}</p>
-    
+    <table>
+        <tr>
+            <th>العنوان</th>
+            <th>القيمة</th>
+        </tr>
+        <tr>
+            <td>التاريخ</td>
+            <td>{{ $date }}</td>
+        </tr>
+        <tr>
+            <td>حجز جزئي العدد</td>
+            <td>{{ $partial_reservation_count }}</td>
+        </tr>
+        <tr>
+            <td>حجز جزئي المبلغ</td>
+            <td>{{ $partial_reservation_amount }}</td>
+        </tr>
+        <tr>
+            <td>حجز كلي العدد</td>
+            <td>{{ $full_reservation_count }}</td>
+        </tr>
+        <tr>
+            <td>حجز كلي المبلغ</td>
+            <td>{{ $full_reservation_amount }}</td>
+        </tr>
+        <tr>
+            <td>اجمالي المبلغ</td>
+            <td>{{ $total_amount }}</td>
+        </tr>
+    </table>
+
     <script>
         window.onload = function() {
             window.print();
