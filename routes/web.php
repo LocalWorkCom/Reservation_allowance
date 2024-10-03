782d3f416f9343682433d47a258dcdbf2519b8c7
Rasha Shalan <rasha.shalan@brmaja.com>
2024-10-03 14:37:53 +0300

    nationality

--- routes/web.php
+++ routes/web.php
@@ -269,6 +269,18 @@ Route::middleware(['auth'])->group(function () {
     Route::get('setting/grads/show/{id}', [settingController::class, 'showgrads'])->name('grads.show')->middleware('check.permission:view grade');
     Route::post('setting/grads/delete', [settingController::class, 'deletegrads'])->name('grads.delete')->middleware('check.permission:delete grade');
     //end grads
+    //start Nationality
+    Route::get('setting/nationality', [settingController::class, 'getAllNationality'])->name('nationality.getAllNationality')->middleware('check.permission:view job');
+    Route::get('setting/nationality/all', [settingController::class, 'indexbationality'])->name('nationality.index')->middleware('check.permission:view job');
+    Route::get('setting/nationality/create', [settingController::class, 'createnationality'])->name('setting.createnationality')->middleware('check.permission:view job');
+
+    Route::post('setting/nationality/add', [settingController::class, 'addNationality'])->name('nationality.add')->middleware('check.permission:edit job');
+    Route::get('setting/nationality/edit', [settingController::class, 'editnationality'])->name('setting.editnationality')->middleware('check.permission:view job');
+
+    Route::post('setting/nationality/update', [settingController::class, 'updatenationality'])->name('nationality.update')->middleware('check.permission:edit job');
+    Route::get('setting/nationality/show/{id}', [settingController::class, 'shownationality'])->name('nationality.show')->middleware('check.permission:view job');
+    Route::post('setting/nationality/delete', [settingController::class, 'deletenationality'])->name('nationality.delete')->middleware('check.permission:delete job');
+    //end nationality
     //Start qualifications -------- Need middleware for gard
     Route::get('setting/qualifications/all', [qualificationController::class, 'index'])->name('qualifications.index')->middleware('check.permission:view Qualification');
     Route::get('setting/qualifications/ajax', [qualificationController::class, 'getqualification'])->name('getAllqualification')->middleware('check.permission:view Qualification');
