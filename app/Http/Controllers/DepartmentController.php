a1066d7356b8d515b75f1df0874b0e7d02d8f326
nod102 <eng.noda102@gmai.com>
2024-11-27 10:24:23 +0200

    edit uuid issue 3

--- app/Http/Controllers/DepartmentController.php
+++ app/Http/Controllers/DepartmentController.php
@@ -77,7 +77,7 @@ class DepartmentController extends Controller
             $sectors = Sector::where('id', $current_department->sector_id)->first();
 
             $data = departements::where('parent_id', null)
-                ->where('sector_id', $sectors->id)
+                ->where('id', $current_department->id)
                 ->orderBy('id', 'desc')
                 ->get();
         }
