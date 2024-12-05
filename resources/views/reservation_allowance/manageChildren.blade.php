4213ebfef362462796279b9e718984583257292b
nod102 <eng.noda102@gmai.com>
2024-12-05 16:08:04 +0200

    edit add all to sector in reservation 5-12-2024 9

--- resources/views/reservation_allowance/manageChildren.blade.php
+++ resources/views/reservation_allowance/manageChildren.blade.php
@@ -1,6 +1,6 @@
 @foreach($children as $child)
 <?php /*<option value="{{$child->id}}" {{$child->id == $departement_id ? "selected" : ""}}>*/?>
-<option value="{{$child->uuid}}" data-name="{{ $child->name }}">
+<option value="{{$child->uuid}}" data-name="{{ $child->name }}" {{$child->id == $departement_id ? "selected" : ""}}>
 {{ $child->name }}
         @if(count($child->children))
         @include('reservation_allowance.manageChildren',['children' => $child->children, 'parent_id'=>$parent_id])
